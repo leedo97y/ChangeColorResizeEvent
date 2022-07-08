6dfeae246eb85e81caf5444fa441032166f53a41
leedo97y <dlehdus1433@gmail.com>
2022-07-09 00:15:44 +0900

    class 수정

--- src/index.js
+++ src/index.js
@@ -7,14 +7,19 @@ const wideSize = widthSize + 100;
 
 function handleResize() {
   let newWidthSize = window.innerWidth;
+
+  const wide = "wide";
+  const narrow = "narrow";
+  const initial = "initial";
+
   if (wideSize < newWidthSize) {
-    body.className = "wide";
+    body.className = wide;
   } else if (narrowSize < newWidthSize && wideSize >= newWidthSize) {
-    body.classList.remove = "wide";
-    body.className = "initial";
+    body.classList.remove = wide;
+    body.className = initial;
   } else if (narrowSize >= newWidthSize) {
-    body.classList.remove = "initial";
-    body.className = "narrow";
+    body.classList.remove = initial;
+    body.className = narrow;
   }
 }
 
